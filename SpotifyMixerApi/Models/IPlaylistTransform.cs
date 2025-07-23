822d069c8232600bc073748c42fb899973a2b5e4
Evan Murphy <esmurphy7@gmail.com>
2025-07-23 15:37:23 -0700

    use newtonsoft

--- SpotifyMixerApi/Models/IPlaylistTransform.cs
+++ SpotifyMixerApi/Models/IPlaylistTransform.cs
@@ -1,12 +1,7 @@
 using System.Collections.Generic;
-using System.Text.Json.Serialization;
 
 namespace SpotifyMixerApi.Models
 {
-    [JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
-    [JsonDerivedType(typeof(TakeTransform), "TakeTransform")]
-    [JsonDerivedType(typeof(AttributeTransform), "AttributeTransform")]
-    [JsonDerivedType(typeof(OrderTransform), "OrderTransform")]
     public interface IPlaylistTransform
     {
         List<Track> Transform(List<Track> tracks);
--- SpotifyMixerApi/Program.cs
+++ SpotifyMixerApi/Program.cs
@@ -7,7 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto;
+});
 
 // Register the repository abstraction
 if (builder.Environment.IsDevelopment())
